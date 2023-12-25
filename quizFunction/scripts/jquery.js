61194c341433bf97439c1aeef758396c9f1e7c9b
Josiah <josiahsamuel1902@gmail.com>
2023-12-25 17:47:40 -0600

    christmas

---
+++ quizFunction/scripts/jquery.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    $("#next-btn").click(function(){
+        let chosenOption = $("input[name='question-options']:checked").val()
+        alert("Chosen Answer was " + chosenOption);
+    });
+
+    //TEST ON CHRISTMAS
+
+}); 
